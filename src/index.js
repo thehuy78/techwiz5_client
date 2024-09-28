5ec904ee17eddde8537ad547ee99322436e0542a
thehuy78 <thehuy7800@gmail.com>
2024-09-28 11:21:35 +0700

    up

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'react-notifications/lib/notifications.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <BrowserRouter basename="/tw5">
+  <BrowserRouter basename="/techwiz5_client">
     <App />
   </BrowserRouter>
 
